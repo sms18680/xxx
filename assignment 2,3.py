f9282213521db73833613d8b1ace799e574ab497
sms18680 <124382156+sms18680@users.noreply.github.com>
2023-02-10 20:59:41 +0530

    Add files via upload

---
+++ assignment 2,3.py
@@ -0,0 +1,25 @@
+# write a pythin programeto count the number of evenand odd.
+
+for i in range(20, 30,2):
+    print(i)
+
+for i in range(30,20,-1):
+    print(i)
+
+
+# write a pythin programe that accept a word from the user and reverse it:
+
+for i in ('mahesh'):
+    print(i) 
+
+for i in reversed('mahesh'):
+    print(i)
+
+
+# replace
+
+r = 'machine learning'
+r.replace('machine', 'engine')
+
+a = 'mahesh sureshbhai shah'
+a.split('_')
